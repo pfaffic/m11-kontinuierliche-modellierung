d8052637996713e5fe908c3e9bbd08d63348c03f
Christoph Pfaffmann <christoph.pfaffmann@outlook.com>
2020-05-05 22:03:28 +0200

    changed f3-f4

--- Tutoriumsaufgabe 2/tutorium2b.m
+++ Tutoriumsaufgabe 2/tutorium2b.m
@@ -8,11 +8,13 @@ step=0.03;
 
 for i=1:(length(X)) 
 for j=1:(length(Y))
-   f(j,i)=1*Quellfunktion(x(j,i),y(j,i)); 
+   f(j,i)=1*Quellfunktion4(x(j,i),y(j,i)); 
  endfor  
+n_Fig=1;
 endfor
-
-for t=1:3
+t_step = 0.05;
+t_max = 3;
+for t=1:t_step:t_max
 for i=1:(length(X)) 
 for j=1:(length(Y))
 xstar=x(j,i)*ones(length(Y),length(X));
@@ -26,15 +28,21 @@ I(j,i) = trapz(Y,trapz(X,psi.*f,2));
  endfor  
 endfor
 
-figure(10+t)
+figure(n_Fig)
 meshc(x,y,I)
-axis([0 2 0 2 0 0.0003])
+axis([0 2 0 2 -0.001 0.001])
 grid on
-title ('Losung von Poissongleichung')
+title (['Losung von Poissongleichung | t=' num2str(t) " | a=" num2str(a)])
 xlabel('x')
-xlabel ('y')
+ylabel ('y')
+  filename=["images/Figure_",num2str(n_Fig),".jpg"];
+  saveas(n_Fig, filename);
+  n_Fig=n_Fig+1;
+  close all;
 endfor
 
 figure(99)
 surfc(x,y,f)
 title ('Funktion der rechten Seite')
+saveas(99, ["images/Figure_",num2str(99),".jpg"]);
+  close all;
